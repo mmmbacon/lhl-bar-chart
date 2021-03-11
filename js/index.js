fbde6745ba0f5d9ce6b928c5f98021ef352374bd
Brandon <bmacdonald1986@gmail.com>
2021-03-11 23:32:10 +0000

    Worked on theming and event listeners for theme links

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
 'use strict';
 
+var themeOverrides = {
+  darkly: {
+    main: 'var(--gray-dark)'
+  }
+}
+
+var currentTheme = "Lightly";
+
 var App = (function () {
 
   var App = function () {
 
     var api = {};
 
-    api.container = $("<div id='app'></div>");
+    api.container = $(`<div id='app'></div>`);
 
     return api;
   }
@@ -14,17 +22,52 @@ var App = (function () {
   return App;
 })();
 
-var Navbar = (function (title) {
+var Navbar = (function (options) {
 
-  var Navbar = function (title) {
+  var Navbar = function (options) {
+
+    var updateTheme = function(type, params){
+      switch(params.theme){
+        case 'Lightly':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('navbar-dark bg-dark').addClass('navbar-light bg-light').fadeIn().delay(200);
+          });
+          break;
+        case 'Darkly':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('navbar-light bg-light').addClass('navbar-dark bg-dark').fadeIn().delay(200);
+          });
+          break;
+        case 'Avocado Toast':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('navbar-dark bg-dark').addClass('navbar-light bg-light').fadeIn().delay(200);
+          });
+          break;
+        case 'Rocket Popsicle':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('navbar-dark bg-dark').addClass('navbar-light bg-light').fadeIn().delay(200);
+          });
+          break;
+      }
+
+      var html = api.container.html();
+    }
+
+    //Event Listeners
+    $(document).on("themeUpdate", updateTheme);
+
+    //Should not be declared inside the class like this, but whatever... time...
+    var themeLink1 = new NavbarThemeLink({ href: '#', text: 'Darkly' });
+    var themeLink2 = new NavbarThemeLink({ href: '#', text: 'Lightly' });
+    var themeLink3 = new NavbarThemeLink({ href: '#', text: 'Avocado Toast' });
+    var themeLink4 = new NavbarThemeLink({ href: '#', text: 'Rocket Popsicle' });
 
     var api = {};
-
-    api.title = title;
+    api.options = options;
     api.container = $(`
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg ${api.options.theme === 'Darkly' ? 'navbar-dark' : 'navbar-light'} ${api.options.theme === 'Darkly' ? 'bg-dark' : 'bg-light'}">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">${api.title}</a>
+          <a class="navbar-brand" href="#">${api.options.title}</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
@@ -42,17 +85,59 @@ var Navbar = (function (title) {
               <li class="nav-item">
                 <a class="nav-link" href="#">Data Set Example 3</a>
               </li>
-            </ul>
-          </div>
-        </div>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Color Themes
+                </a>
+                <ul id="themeList" class="dropdown-menu" aria-labelledby="navbarDropdown"></ul>
+              </li>
       </nav>
     `);
+
+    api.container.find("#themeList").append(themeLink1.container);
+    api.container.find("#themeList").append(themeLink2.container);
+    api.container.find("#themeList").append(themeLink3.container);
+    api.container.find("#themeList").append(themeLink4.container);
 
     return api;
 
   };
 
   return Navbar;
+})();
+
+var NavbarThemeLink = (function (options) {
+
+  var NavbarThemeLink = function (options) {
+
+    var api = {};
+    api.options = options;
+    api.container = $(`<a class="dropdown-item" href="#">${api.options.text}</a>`).on('click', function(){
+      switch(api.options.theme){
+        case 'Lightly':
+          currentTheme = "Lightly";
+          break;
+        case 'Darkly':
+          currentTheme = "Darkly";
+          break;
+        case 'Avocado Toast':
+          currentTheme = "Avocado Toast";
+          break;
+        case 'Rocket Popsicle':
+          currentTheme = "Rocket Popsicle";
+          break;
+      }
+
+      $.event.trigger("themeUpdate", {
+        theme: api.options.text
+      });
+    });
+
+    return api;
+
+  };
+
+  return NavbarThemeLink;
 })();
 
 var Header = (function (title) {
@@ -115,9 +200,39 @@ var Footer = (function (title) {
   return Footer;
 })();
 
-var Page = (function () {
+var Page = (function (options) {
 
-  var Page = function () {
+  var Page = function (options) {
+
+    var updateTheme = function(type, params){
+      switch(params.theme){
+        case 'Lightly':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('bg-dark').addClass('bg-light').fadeIn().delay(200);
+          });
+          break;
+        case 'Darkly':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('bg-light').addClass('bg-dark').fadeIn().delay(200);
+          });
+          break;
+        case 'Avocado Toast':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('bg-dark').addClass('bg-light').fadeIn().delay(200);
+          });
+          break;
+        case 'Rocket Popsicle':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('bg-dark').addClass('bg-light').fadeIn().delay(200);
+          });
+          break;
+      }
+
+      var html = api.container.html();
+    }
+
+    //Event Listeners
+    $(document).on("themeUpdate", updateTheme);
 
     var _style = `"
     "`
@@ -125,8 +240,8 @@ var Page = (function () {
     var api = {};
 
     api.container = $(`
-      <div class="container-fluid" style=${_style}>
-        This is a page
+      <div id="page" class="container-fluid bg-light" style=${_style}>
+
       </div>
       `);
 
@@ -153,15 +268,16 @@ var Barchart = (function (data, options) {
 
     var generateItems = function (items) {
 
+      var maxValue = Math.max.apply(Math, items);
+      var itemWidth = (1 / items.length) * 100;
+
       if (items) {
         const listItems = items.map((item) => {
 
-          var chartItem = new BarchartItem(item, { bgcolor: 'blue' });
-          return chartItem.container;
-
-        }).join("");
-
-        return listItems;
+          var itemHeight = (item / maxValue) * api.options.maxChartHeight;
+          var chartItem = new BarchartItem(item, { bgcolor: 'blue', width: itemWidth, height: itemHeight });
+          api.container.find("#barchart_chart").append(chartItem.container);
+        });
       }
 
     }
@@ -176,18 +292,36 @@ var Barchart = (function (data, options) {
         <div id="barchart_heading"></div>
         <div id="barchart_content">
           <div id="barchart_left">
-            <div id="barchart_yAxis">Y-Axis</div>
+            <div id="barchart_yAxis">
+              <div class="row">
+                <div class="col">
+                  <h6>
+                    <p class="text-center my-2">${api.options.yAxisTitle}</p>
+                  </h6>
+                </div>
+              </div>
+            </div>
           </div>
           <div id="barchart_main" style=${_style.main}>
             <div id="barchart_chart" style=${_style.chart}>
-              ${generateItems(data)}
+
             </div>
-            <div id="barchart_xAxis">X_Axis</div>
+            <div id="barchart_xAxis" class="container">
+              <div class="row">
+                <div class="col">
+                  <h6>
+                    <p class="text-center my-2">${api.options.xAxisTitle}</p>
+                  </h6>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div id="barchart_footer"></div>
       </div>
-      `);
+    `);
+
+    generateItems(api.data);
 
     return api;
 
@@ -200,19 +334,49 @@ var BarchartItem = (function (data, options) {
 
   var BarchartItem = function (data, options) {
 
+    var updateTheme = function(type, params){
+      switch(params.theme){
+        case 'Lightly':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('bg-primarytext-light').addClass('bg-light border text-dark').fadeIn().delay(200);
+          });
+          break;
+        case 'Darkly':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('bg-light border text-dark').addClass('bg-primary text-light').fadeIn().delay(200);
+          });
+          break;
+        case 'Avocado Toast':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('bg-primarytext-light').addClass('bg-light border text-dark').fadeIn().delay(200);
+          });
+          break;
+        case 'Rocket Popsicle':
+          api.container.fadeOut(200, function(){
+            api.container.removeClass('bg-primarytext-light').addClass('bg-light border text-dark').fadeIn().delay(200);
+          });
+          break;
+      }
+
+      var html = api.container.html();
+    }
+
+    //Event Listeners
+    $(document).on("themeUpdate", updateTheme);
+
     var _style = {
       bar: `"
         display: inline-block;
         background-color: var(--bs-light);
-        width: ${100 * data}px;
-        height: ${data * 20}px;
+        width: ${options.width}%;
+        height: ${options.height}px;
         text-align: center;
       "`
     }
 
     var api = {};
 
-    api.container = `<span class="border" style=${_style.bar}>${data}</span>`;
+    api.container = $(`<span class="border mx-1 bg-light" style=${_style.bar}>${data}</span>`);
 
     return api;
 
@@ -223,13 +387,13 @@ var BarchartItem = (function (data, options) {
 
 $(document).ready(function () {
 
-  var testData = [1, 2, 3, 4, 5, 6, 7];
+  var testData = [0.3, 21, 33, 75, 98, 100];
 
   var app = new App();
-  var navbar = new Navbar("Barchart App");
+  var navbar = new Navbar({ title: "Barchart App" });
   var footer = new Footer("Lighthouse Labs Barchart App - Brandon Macdonald 2021");
   var mainpage = new Page();
-  var barchart = new Barchart(testData);
+  var barchart = new Barchart(testData, { xAxisTitle: "Students", yAxisTitle: "Strength", maxChartHeight: 500 });
 
   mainpage.container.append(barchart.container);
 
